906747bcfce6d770b4f20ac9e100549a3e4274ad
Nabeel <nabeel.sohail2630@outlook.com>
2022-10-23 21:23:48 +0500

    server created

--- server.mjs
+++ server.mjs
@@ -1,3 +1,17 @@
-console.log(
-    "server is running"
-);
+import express from 'express';
+const app = express()
+const port = 3000
+
+app.get('/water', (req, res) => {
+    console.log(`${req.ip} is requesting`);
+    res.send('Here is some water for you')
+})
+
+app.get('/food', (req, res) => {
+    console.log(`${req.ip} is requesting`);
+    res.send('Here is some food for you')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
