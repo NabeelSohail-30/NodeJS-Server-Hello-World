2a38cccaf402ddf8c559cf89930a1f7962918823
Nabeel <nabeel.sohail2630@outlook.com>
2022-10-23 21:31:36 +0500

    made some changes

--- server.mjs
+++ server.mjs
@@ -7,9 +7,39 @@ app.get('/water', (req, res) => {
     res.send('Here is some water for you')
 })
 
-app.get('/food', (req, res) => {
+app.get('/breakfast', (req, res) => {
     console.log(`${req.ip} is requesting`);
-    res.send('Here is some food for you')
+    res.send('Here is some breakfast for you')
+})
+
+app.get('/lunch', (req, res) => {
+    console.log(`${req.ip} is requesting`);
+    res.send('Here is some lunch for you')
+})
+
+app.get('/dinner', (req, res) => {
+    console.log(`${req.ip} is requesting`);
+    res.send('Here is some dinner for you')
+})
+
+app.get('/juice', (req, res) => {
+    console.log(`${req.ip} is requesting`);
+    res.send('Here is some juice for you')
+})
+
+app.get('/milk', (req, res) => {
+    console.log(`${req.ip} is requesting`);
+    res.send('Here is some milk for you')
+})
+
+app.get('/tea', (req, res) => {
+    console.log(`${req.ip} is requesting`);
+    res.send('Here is some tea for you')
+})
+
+app.get('/coffee', (req, res) => {
+    console.log(`${req.ip} is requesting`);
+    res.send('Here is some coffee for you')
 })
 
 app.listen(port, () => {
